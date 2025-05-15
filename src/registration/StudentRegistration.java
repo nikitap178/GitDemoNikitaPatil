733a980d727eaa841452a054231b7d519570f323
nikitapatil178 <nikitapatil2017@gmail.com>
2025-05-15 18:51:32 +0530

    Create a new project

--- src/registration/StudentRegistration.java
+++ src/registration/StudentRegistration.java
@@ -5,6 +5,7 @@ public class StudentRegistration {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("HelloWorld");
 	}
 
 }
